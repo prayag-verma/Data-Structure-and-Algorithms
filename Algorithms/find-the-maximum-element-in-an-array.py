fd68503b72f27cd760c561c5ca04570f12845ec6
Prayag Verma <prayagpverma@gmail.com>
2025-07-18 00:12:41 -0500

    Optimized

--- Algorithms/find-the-maximum-element-in-an-array.py
+++ Algorithms/find-the-maximum-element-in-an-array.py
@@ -2,12 +2,12 @@ ar = [3, 17, 2, 10, 8]
 
 print(f"Original array list:", ar)
 
-# Method 1: Using the max() function to find the maximum element
+# Method 1: Simply using max() function to find the maximum element
 print("Maximum element in the array using max() function:", max(ar))
 
+# Method 2: Simply sorting the array in ascending order and printing the last element ar [-1]
 ar.sort(reverse=False)
 
-# Method 2: Simply sorting the array in ascending order and printing the last element ar [-1]
 print(f"Last element of array list-sorted in ascending order:", ar[-1])
 
 # Method 3: Simply printing the last element of the sorted array using a loop
