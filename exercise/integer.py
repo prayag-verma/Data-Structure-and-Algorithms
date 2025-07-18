f1e8579f8a1ca1094db8fe553dadd9eb35b71da0
Prayag Verma <prayagpverma@gmail.com>
2025-07-18 00:23:37 -0500

    Added excercise directory

---
+++ exercise/integer.py
@@ -0,0 +1,9 @@
+# Python Integer data type - practice
+
+a = 5
+b = 3
+print(a + b)  # Output: 8
+print(type(a))  # Output: <class 'int'>
+
+c = int(input("Press Enter any number..."))
+print(a+c)  
